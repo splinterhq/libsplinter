a4ebf3256a73ee3d46b817ee8e22cd844b9d8519
= <=>
2026-01-29 14:32:48 -0500

    cli: get should be type aware

--- splinter_cli_cmd_get.c
+++ splinter_cli_cmd_get.c
@@ -26,6 +26,7 @@ int cmd_get(int argc, char *argv[]) {
     char key[SPLINTER_KEY_MAX] = { 0 };
     char *tmp = getenv("SPLINTER_NS_PREFIX");
     size_t received = 0;
+    splinter_slot_snapshot_t snap = { 0 };
     int rc = -1;
 
     if (argc != 2) {
@@ -35,14 +36,28 @@ int cmd_get(int argc, char *argv[]) {
     
     snprintf(key, sizeof(key) -1, "%s%s", tmp == NULL ? "" : tmp, argv[1]);
 
+    // 1. Get the data
     rc = splinter_get(key, buf, sizeof(buf), &received);
     if (rc != 0) {
         fprintf(stderr, "%s: unable to retrieve key '%s'\n", modname, key);
         return rc;
     }
 
-    printf("%lu:%s\n", received, buf);
-    puts("");
+    // 2. Get the type metadata
+    if (splinter_get_slot_snapshot(key, &snap) != 0) {
+        // Fallback to string if snapshot fails
+        printf("%lu:%s\n", received, buf);
+    } else {
+        // 3. Type-Aware Printing
+        if (snap.type_flag & SPL_SLOT_TYPE_BIGUINT) {
+            uint64_t val = *(uint64_t *)buf;
+            printf("%lu:%llu\n", received, (unsigned long long)val);
+        } else {
+            // Standard string output
+            printf("%lu:%s\n", received, buf);
+        }
+    }
 
+    puts("");
     return 0;
 }
