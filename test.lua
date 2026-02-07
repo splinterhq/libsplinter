5c6eb220438c481080d6a7a68298b85566bad3d9
= <=>
2026-02-06 20:38:27 -0500

    cli: lua: little test script

--- test.lua
+++ test.lua
@@ -1,5 +1,13 @@
 -- get and print a known key (quick lua check)
+
 local bus = require("splinter")
 local test = bus.get("test_key") or 0
 print("Test result:" .. test)
+
+local multi = "1, 2, 3, 4, 5"
+local result = bus.set("test_multi", multi) or -1
+print(result)
+
+bus.set("test_integer", 1)
+bus.math("test_integer", "inc", 65535)
 
