9ede44007e90303ca140cc50066948a7e49eeff6
= <=>
2026-04-25 16:21:42 -0400

    cli: provide recursive  option for removing tandem keys

--- splinter_cli_cmd_unset.c
+++ splinter_cli_cmd_unset.c
@@ -17,7 +17,9 @@ static const char *modname = "unset";
 void help_cmd_unset(unsigned int level) {
     (void) level;
     printf("%s un-sets the value of a key in the store\n", modname);
-    printf("Usage: %s <key_name>\n", modname);
+    printf("Usage: %s [-r] <key_name>\n", modname);
+    printf("  -r  recursively remove tandem keys (key%s1, key%s2, ...)\n",
+           SPL_ORDER_ACCESSOR, SPL_ORDER_ACCESSOR);
     return;
 }
 
@@ -25,17 +27,43 @@ int cmd_unset(int argc, char *argv[]) {
     size_t deleted = 0;
     char key[SPLINTER_KEY_MAX] = { 0 };
     char *tmp = getenv("SPLINTER_NS_PREFIX");
+    int recursive = 0;
+    const char *key_arg = NULL;
 
-    if (argc != 2) {
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-r") == 0) {
+            recursive = 1;
+        } else if (key_arg == NULL) {
+            key_arg = argv[i];
+        } else {
+            help_cmd_unset(1);
+            return 1;
+        }
+    }
+
+    if (key_arg == NULL) {
         help_cmd_unset(1);
         return 1;
     }
-    snprintf(key, sizeof(key) - 1, "%s%s", tmp == NULL ? "" : tmp, argv[1]);
 
-    deleted = splinter_unset(key);
-    if ((int) deleted < 0)
-        return (int) deleted;
-    
+    snprintf(key, sizeof(key) - 1, "%s%s", tmp == NULL ? "" : tmp, key_arg);
+
+    int ret = splinter_unset(key);
+    if (ret < 0)
+        return ret;
+    deleted += (size_t) ret;
+
+    if (recursive) {
+        char tandem_key[SPLINTER_KEY_MAX];
+        for (uint8_t i = 1; i != 0; i++) {
+            snprintf(tandem_key, sizeof(tandem_key) - 1, "%s%s%u", key, SPL_ORDER_ACCESSOR, i);
+            ret = splinter_unset(tandem_key);
+            if (ret < 0)
+                break;
+            deleted += (size_t) ret;
+        }
+    }
+
     printf("%lu bytes deleted.\n", deleted);
 
     return 0;
